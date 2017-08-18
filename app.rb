8d7ce8149612b641e6be9122aa77000c06fc9535
yoog84 <yoog84@rambler.ru>
2017-08-18 12:32:32 +0300

    add client entity(sozdanie sushnosty)

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barbershop.db"#spzdaem podkluchenie k BD(esli menyaetsya BD,menyaetsya & stroka)
+
+#sozdaem class kotory budet predstavlyat nashu sushnost
+class Client <ActiveRecord::Base#class nasleduetsya ot activerecord base(vse s bolsh bukvi)
+	end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
