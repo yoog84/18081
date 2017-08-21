6f3bd5ddd37e0c53304b84a85579160e8479222d
yoog84 <yoog84@rambler.ru>
2017-08-21 23:56:39 +0300

    add index erb

--- app.rb
+++ app.rb
@@ -14,5 +14,5 @@ class Barber <ActiveRecord::Base#class nasleduetsya ot activerecord base(vse s b
 	end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index
 end
